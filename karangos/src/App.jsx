04e0d1c8f1c8e6a5e9be283fb244e0e1a91e9402
João Pedro Freitas Cintra <101219212+JoaoPedroCintra@users.noreply.github.com>
2023-11-22 21:09:01 +0000

    up

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -1,26 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import HeaderBar from './ui/HeaderBar'
-import './App.css'
-import {ThemeProvider} from '@mui/material/styles'
-import theme from './utils/theme'
-import Box from '@mui/material/Box'
-
-function App() {
-  
-
-  return (
-    <>
-    <ThemeProvider theme={theme}>
-      <Box sx={{
-        width: '100vw', // toda a largura da janela
-        minHeight: '100vh', // no mínimo, toda a altura da janela
-        backgroundColor:'background.default' // cinza escuro
-      }}>
-        <HeaderBar/>
-      </Box>
-    </ThemeProvider>
-=======
 import './App.css'
 import HeaderBar from './ui/HeaderBar'
 import { ThemeProvider } from '@mui/material/styles'
@@ -57,7 +35,6 @@ function App() {
           </Box>
         </ThemeProvider>
       </BrowserRouter>
->>>>>>> d9c0c456e6f2adf388e9867d0429adb4b6f3f7c2
     </>
   )
 }
--- karangos/src/ui/MainMenu.jsx
+++ karangos/src/ui/MainMenu.jsx
@@ -3,10 +3,7 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-<<<<<<< HEAD
-=======
 import { Link } from 'react-router-dom'
->>>>>>> d9c0c456e6f2adf388e9867d0429adb4b6f3f7c2
 
 export default function MainMenu() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -20,17 +17,6 @@ export default function MainMenu() {
 
   return (
     <div>
-<<<<<<< HEAD
-        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}
-            id="basic-button"
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-        >
-            <MenuIcon />
-        </IconButton>
-=======
       <IconButton 
         edge="start" 
         color="inherit" 
@@ -43,7 +29,6 @@ export default function MainMenu() {
       >
         <MenuIcon />
       </IconButton>
->>>>>>> d9c0c456e6f2adf388e9867d0429adb4b6f3f7c2
       <Menu
         id="basic-menu"
         anchorEl={anchorEl}
@@ -53,11 +38,6 @@ export default function MainMenu() {
           'aria-labelledby': 'basic-button',
         }}
       >
-<<<<<<< HEAD
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-=======
         <MenuItem 
           onClick={handleClose}
           component={Link}
@@ -82,7 +62,6 @@ export default function MainMenu() {
         >
           Clientes
         </MenuItem>
->>>>>>> d9c0c456e6f2adf388e9867d0429adb4b6f3f7c2
       </Menu>
     </div>
   );
